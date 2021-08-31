9a75a4d27fb8045cf283d9aa66e6f258173773a5
shorefb <reid.vanwagner@gmail.com>
2021-08-31 09:04:15 -0400

    final commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com'],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoic2hvcmVmYiIsImEiOiJja3QwMjNkczEwMTJtMnZtaGppbmp1Mmx0In0.ymgrBQsBWdzY31qm4vHE-Q'
     }
+
 }
